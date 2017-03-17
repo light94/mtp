10417a7c8343f7193281eb34882570d1d517d7e0
Rahul Mishra <mishra.rahul1712@gmail.com>
2017-03-17 22:41:39 +0530

    Calculate returns

--- script.py
+++ script.py
@@ -10,3 +10,12 @@ for stock in stock_list:
     data = share.get_historical(START_DATE, END_DATE)
     price_data = [daily_data['Close'] for daily_data in data]
     historical_data[stock] = pd.Series(price_data)
+
+historical_data_shift = historical_data.iloc[1:,:].append(historical_data.tail(1))
+historical_data_shift.reset_index(inplace=True, drop=True)
+# convert to floats
+historical_data = historical_data.astype(float)
+historical_data_shift = historical_data_shift.astype(float)
+
+
+returns = historical_data_shift / historical_data
