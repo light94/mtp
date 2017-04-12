f885aa7899b8c7ae743c587d2c3aeaf1fb810757
Rahul Mishra <mishra.rahul1712@gmail.com>
2017-04-12 14:34:43 +0530

    Comparison portfolios

---
+++ compare.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import operator
+
+def equally_weighted_portfolio(returns, k):
+    return np.sum(returns.iloc[k,:])/num_stocks
+
+
+eq_returns = []
+for k in range(BACKUP,num_rows):
+    eq_returns.append(equally_weighted_portfolio(returns,k))
+
+
+def get_cumulative_wealth():
+    cw = 1
+    for i in range(len(eq_returns)):
+        cw *= (1+eq_returns[i]/100)
+    return cw
+
+nn = pd.DataFrame()
+nn['bandit'] = realized_return_list
+nn['equal'] = eq_returns
+nn['diff=bandit-equal'] = map(operator.sub,realized_return_list,eq_returns)
+nn.to_csv('results.csv', index=False)   
+cw2 = get_cumulative_wealth()
